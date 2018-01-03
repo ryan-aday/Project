cc3bfb6f7f9d896c45c4de22a35c945f0d247a39
ryan-aday <raday@stuy.edu>
2018-01-03 15:22:13 -0500

    Broken mainMenu

---
+++ mainMenu.java
@@ -0,0 +1,34 @@
+import java.awt.image.BufferedImage;
+import javax.swing.*;
+import java.awt.*;
+
+import java.io.*;
+import javax.imageio.ImageIO;
+
+public class mainMenu extends JFrame{
+    private Container pane;
+    private JButton PlayGame;
+    private JLabel background;
+
+    private void mouseClicked(MouseEvent e){
+    }
+
+    private void mouseEntered(MouseEvent e){
+    }
+
+    private void mousePressed(MouseEvent e){
+    }
+
+    private void mouseReleased(MouseEvent e){
+    }
+
+    private void ActionPerformed(ActionEvent ae){
+    }
+
+    private mainMenu(){}
+
+    private static void main (String[] args){
+    }
+    
+}
+   
