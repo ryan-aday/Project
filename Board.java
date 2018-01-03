8122d3fde246c0c259d434b8ce071f527da36562
Yuxuanzou <yzou2@stuy.edu>
2018-01-03 15:51:33 -0500

    Started working on the constructors but didn't have enough time to finish in class

--- Board.java
+++ Board.java
@@ -5,13 +5,22 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Board{
+public class Board extends JFrame{
     private Container pane;
     private JButton[][] Locations;
     private JButton endTurn;
 
     public Board(){
 	Locations = new JButton[8][8];
+	pane = this.getContentPane();
+	endTurn = new JButton("End Turn");
+
+	pane.add(endTurn);
+        for (int i = 0;i < Locations.length;i ++){
+	    for (int c = 0; c < Locations[i].length;c ++){
+		
+	    }
+	}
     }
     
     public void actionPerformed(ActionEvent e){
