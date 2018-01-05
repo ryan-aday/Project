c7cb26f0abce46ee571c895fcdce3ee5936fc9d1
ryan-aday <raday@stuy.edu>
2018-01-05 16:52:57 -0500

    Still working on implementing Board with mainMenu, apparently clearing method didn't work

--- mainMenu.java
+++ mainMenu.java
@@ -6,9 +6,9 @@ import java.awt.event.*;
 import java.io.*;
 import javax.imageio.ImageIO;
 
-public class mainMenu extends JFrame{
+public class mainMenu extends Board{
     private Container pane;
-    private JPanel game;
+    //private JPanel game;
     private JButton StartGame;
     private JLabel background;
 
@@ -33,6 +33,7 @@ public class mainMenu extends JFrame{
     }
 
     public mainMenu(){
+	getContentPane().removeAll();
         this.setTitle("Othello: The Game");
 	this.setSize(850,300);
 	this.setLocation(100,100);
@@ -63,24 +64,20 @@ public class mainMenu extends JFrame{
 	StartGame.addActionListener(new ActionListener(){
 		public void actionPerformed(ActionEvent e)
 		{
-		    getContentPane().removeAll();
+		    //getContentPane().removeAll();
+		    //super.Board();
+		    
+		    /*
 		    getContentPane().add(game, BorderLayout.CENTER);
 		    getContentPane().doLayout();
 		    update(getGraphics());
+		    */
 		}
 	    });
 		    
 	
         pane.add(StartGame);
 
-    }
-
-    
-    private void changePanel(JPanel panel) {
-	getContentPane().removeAll();
-	getContentPane().add(panel, BorderLayout.CENTER);
-	getContentPane().doLayout();
-	update(getGraphics());
     }
     
     public static void main(String[] args) {
