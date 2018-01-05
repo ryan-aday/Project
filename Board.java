0dd2493e16e935146a330b92cb9e53afd8b1f5bd
Yuxuanzou <yzou2@stuy.edu>
2018-01-04 17:24:09 -0700

    Successfully setup the board

--- Board.java
+++ Board.java
@@ -4,6 +4,8 @@ import javax.swing.JComponent;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.border.LineBorder;
+import javax.swing.ImageIcon;
 
 public class Board extends JFrame{
     private Container pane;
@@ -11,16 +13,20 @@ public class Board extends JFrame{
     private JButton endTurn;
 
     public Board(){
+    pane = this.getContentPane();
 	Locations = new JButton[8][8];
-	pane = this.getContentPane();
-	endTurn = new JButton("End Turn");
+    pane.setLayout(new GridLayout(9,9));
 
-	pane.add(endTurn);
-        for (int i = 0;i < Locations.length;i ++){
-	    for (int c = 0; c < Locations[i].length;c ++){
-		
+    for (int i = 0;i < Locations.length;i ++){
+	   for (int c = 0; c < Locations[i].length;c ++){
+            Locations[i][c] = new JButton();
+		    Locations[i][c].setBackground(new Color(0,153,76));
+            Locations[i][c].setBorder(new LineBorder(Color.BLACK));
+            pane.add(Locations[i][c]);
 	    }
 	}
+    endTurn = new JButton("End Turn");
+    pane.add(endTurn);
     }
     
     public void actionPerformed(ActionEvent e){
@@ -33,6 +39,8 @@ public class Board extends JFrame{
 
     public static void main(String[] args){
         Board g = new Board();
-	g.setVisible(true);
+        g.setSize(new Dimension(800,800));
+        g.setVisible(true);
+        g.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 }
