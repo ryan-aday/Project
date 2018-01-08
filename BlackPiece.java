49f9ea2dfe3af8b6f260e85f6dffc0d63ff4e14e
ryan-aday <raday@stuy.edu>
2018-01-07 23:26:52 -0500

    Edited Pieces and wrtoe some methods for BlackPieces.  The code might end up diverging from the UML.

--- BlackPiece.java
+++ BlackPiece.java
@@ -1,22 +1,49 @@
 
-public BlackPiece extends Pieces{
+public class BlackPiece extends Pieces{
 
     private int x, y;
-    private int color;
+    private int color=0;
     private int numValidMoves;
-    private int[][] allPoints
+    private int[] allPoints={};
 
-    private void showValidMoves(){}
+    public void showValidMoves(){}
 
-    private int getColor(){}
+    public void setColor(int x, int y){
+	super.colors[x][y]=0;
+    }
+
+    public void addToPoints(int x, int y){
+	int[] point={x+10*y};
+        int[] comb= new int[allPoints.length+1];
+	for (int count=0; count<comb.length-1; count++){
+	    comb[count]=allPoints[count];
+	}comb[comb.length-1]=point[0];
+	allPoints=comb;
+    }
     
-    private void setColor(){}
+    public int getX(int loc){
+	for (int rcount=0; rcount<8; rcount++){
+	    for (int ccount=0; ccount<8; ccount++){
+		if (super.locations[rcount][ccount]==loc){
+		    return rcount;
+		}
+	    }
+	}
+	return -1;
+    }
 
-    private void addToPoints(){}
-    
-    private int getX(){}
+    public int getY(int loc){
+	for (int rcount=0; rcount<8; rcount++){
+	    for (int ccount=0; ccount<8; ccount++){
+		if (super.locations[rcount][ccount]==loc){
+		    return ccount;
+		}
+	    }
+	}
+	return -1;
+    }
 
-    private int getY(){}
+    public void flip(){}
 
     public BlackPiece(int x, int y){}
 }
--- Pieces.java
+++ Pieces.java
@@ -1,12 +1,31 @@
-public abstract Pieces{
-    public abstract void showValidMoves(){}
+public abstract class Pieces{
+    public int[][] locations={ {11, 12, 13, 14, 15, 16, 17, 18},
+				{21, 22, 23, 24, 25, 26, 27, 28},
+				{31, 32, 33, 34, 35, 36, 37, 38},
+				{41, 42, 43, 44, 45, 46, 47, 48},
+				{51, 52, 53, 54, 55, 56, 57, 58},
+				{61, 62, 63, 64, 65, 66, 67, 68},
+				{71, 72, 73, 74, 75, 76, 77, 78},
+				{81, 82, 83, 84, 85, 86, 87, 88}};
+    public int[][] colors= new int[8][8];
+    
+    abstract void showValidMoves();
 
-    private abstract int getColor(){}
+    private int getColor(int loc){
+	for (int rcount=0; rcount<8; rcount++){
+	    for (int ccount=0; ccount<8; ccount++){
+		if (locations[rcount][ccount]==loc){
+		    return colors[rcount][ccount];
+		}
+	    }
+	}
+	return -1;
+    }
 
-    private abstract void setColor(){}
+    abstract void setColor(int x, int y);
 
-    private abstract void addToPoints(){}
+    abstract void addToPoints(int x, int y);
 
-    private abstract void flip{}
+    abstract void flip();
 
 }
