19bc5bdb139dad0b934974a28ecdd1a6242dd927
Yuxuanzou <yzou2@stuy.edu>
2018-01-08 21:35:34 -0700

    change it into a non abstract class and made a constructor

--- Pieces.java
+++ Pieces.java
@@ -1,30 +1,18 @@
-public abstract class Pieces{
-    public int[][] locations={ {11, 12, 13, 14, 15, 16, 17, 18},
-				{21, 22, 23, 24, 25, 26, 27, 28},
-				{31, 32, 33, 34, 35, 36, 37, 38},
-				{41, 42, 43, 44, 45, 46, 47, 48},
-				{51, 52, 53, 54, 55, 56, 57, 58},
-				{61, 62, 63, 64, 65, 66, 67, 68},
-				{71, 72, 73, 74, 75, 76, 77, 78},
-				{81, 82, 83, 84, 85, 86, 87, 88}};
-    public int[][] colors= new int[8][8];
-    
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JComponent;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.border.LineBorder;
+import javax.swing.ImageIcon;
 
-    private int getColor(int loc){
-	for (int rcount=0; rcount<8; rcount++){
-	    for (int ccount=0; ccount<8; ccount++){
-		if (locations[rcount][ccount]==loc){
-		    return colors[rcount][ccount];
-		}
-	    }
-	}
-	return -1;
+public class Pieces extends JButton{
+    private int x;
+    private int y;
+    public Pieces(int x, int y){
+        this.x = x;
+        this.y = y;
     }
-
-    abstract void setColor(int x, int y);
-
-    abstract void addToPoints(int x, int y);
-
-    abstract void flip();
 
 }
