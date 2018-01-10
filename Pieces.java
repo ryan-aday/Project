a2c863590577be1f3dbf0dfac9aca61b0758c25f
Yuxuanzou <yzou2@stuy.edu>
2018-01-09 21:05:28 -0700

    added a new field called color, added getX and getY method to the Pieces class

--- Pieces.java
+++ Pieces.java
@@ -10,9 +10,23 @@ import javax.swing.ImageIcon;
 public class Pieces extends JButton{
     private int x;
     private int y;
+    private Color c;
+    
     public Pieces(int x, int y){
         this.x = x;
         this.y = y;
+    }
+    
+    public int getX(){
+        return x;
+    }
+    
+    public int getY(){
+        return y;
+    }
+    
+    public void setColor(Color b){
+        c = b;
     }
 
 }
