b17e25658b9c262e6dcce965fbac94831820dd15
Yuxuanzou <yzou2@stuy.edu>
2018-01-09 21:58:34 -0700

    changed the constructor

--- Pieces.java
+++ Pieces.java
@@ -10,28 +10,9 @@ import javax.swing.ImageIcon;
 public class Pieces extends JButton{
     private int x;
     private int y;
-    private int BlackOrWhite; // 0 is black, 1 is white
     
-    public Pieces(int x, int y, int l){
+    public Pieces(int x, int y){
         this.x = x;
         this.y = y;
-        BlackOrWhite = l;
     }
-    
-    public int getX(){
-        return x;
-    }
-    
-    public int getY(){
-        return y;
-    }
-    
-    public void setColor(int l){
-        BlackOrWhite = l;
-    }
-    
-    public int getColor(){
-        return BlackOrWhite;
-    }
-
 }
