d3e8637716d624a5fc4adce2807ec7cfe8905f95
ryan-aday <raday@stuy.edu>
2018-01-11 14:57:46 -0500

    Added missing Pieces class

---
+++ Pieces.java
@@ -0,0 +1,20 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JComponent;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.border.LineBorder;
+import javax.swing.ImageIcon;
+
+public class Pieces extends JButton{
+    private int x;
+    private int y;
+
+    
+    
+    public Pieces(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
