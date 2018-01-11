66f2daee7aeda9b64361d02cac2f2089e90ac7e8
ryan-aday <raday@stuy.edu>
2018-01-11 15:21:59 -0500

    Modifying Pieces to have get/setColor

--- Pieces.java
+++ Pieces.java
@@ -10,8 +10,8 @@ import javax.swing.ImageIcon;
 public class Pieces extends JButton{
     private int x;
     private int y;
-
     
+    private int color;
     
     public Pieces(int x, int y){
         this.x = x;
@@ -24,5 +24,16 @@ public class Pieces extends JButton{
 
     public int getCol(){
 	return x;
+    }
+
+    public int getColor(){
+	return color;
+    }
+
+    
+    public void setColor(){
+	if (Board.getPlayer()=1){
+	    color=0;
+	}else color=1;
     }
 }
