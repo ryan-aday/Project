44170ef653b572879510a0418f31c4de2544cf30
ryan-aday <raday@stuy.edu>
2018-01-14 21:25:50 -0500

    Optimized Victory so that popup displays once victory conditions on Board are met.  Tells Victor and how many Pieces scored for each player.  Closes on Back to Main Menu, which was initial problem before.

--- Victory.java
+++ Victory.java
@@ -9,12 +9,17 @@ import javax.imageio.ImageIO;
 public class Victory extends JFrame{
     private Container pane;
 
+    private JButton toMain;
+    private JLabel background;
+
     private ImageIcon logo;
     private JLabel logoLabel;
     private JLabel l;
 
+    private JTextField victor, numB, numW;
+
     public Victory(){
-	this.setTitle("Othello: The Game");
+        this.setTitle("Othello: The Game");
 	this.setSize(850,300);
 	this.setLocation(100,100);
 	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -27,11 +32,62 @@ public class Victory extends JFrame{
 	    logo = new ImageIcon(img);
 	    JLabel logoDisp= new JLabel(logo);
 	    JOptionPane.showMessageDialog(null, logoDisp);
+	    //pane.add(logoDisp);
 	}
 
 	catch(IOException ie){
 	    System.out.println("Error reading logo img file");
 	}
+
+	pane.add(Box.createHorizontalGlue());
+        pane.add(Box.createRigidArea(new Dimension(10, 0)));
+
+	String v="";
+	if (Board.checkB()>Board.checkW()){
+	    v="BLACK";
+	}else v="WHITE";
+	
+	
+	victor= new JTextField("VICTOR: " +v);
+	pane.add(victor);
+
+
+	numB= new JTextField("Number of Black Pieces: "+Board.checkB());
+	pane.add(numB);
+
+	numW= new JTextField("Number of White Pieces: "+Board.checkW());
+	pane.add(numW);
+
+	toMain=new JButton("Back to Main Menu");
+	toMain.addActionListener(new ActionListener(){
+		public void actionPerformed(ActionEvent e)
+		{
+		    CloseFrame();
+		    mainMenu.main(new String[0]);
+		}
+	    });
+		    	
+        pane.add(toMain);
+
+    }
+
+    private void mouseClicked(MouseEvent e){
+    }
+   
+    private void mouseEntered(MouseEvent e){
+    }
+    
+    private void mousePressed(MouseEvent e){
+    }
+    
+    private void mouseReleased(MouseEvent e){
+    }
+    
+    private void ActionPerformed(ActionEvent ae){
+    }
+
+    public void CloseFrame(){
+	super.dispose();
     }
     
     public static void main(String[] args) {
@@ -39,4 +95,4 @@ public class Victory extends JFrame{
 	g.setVisible(true);
     } 
 }
- 
+   
