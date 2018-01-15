20b8a0956712727bec2cedf65bf867c9948ed653
ryan-aday <raday@stuy.edu>
2018-01-15 03:11:09 -0500

    Demo version of Board

--- Board.java
+++ Board.java
@@ -82,7 +82,7 @@ public class Board extends JFrame implements MouseListener{
 
 	//Demo Button to simulate a total Black Win
 
-	/*JButton demoVictoryB= new JButton("BWin");
+	JButton demoVictoryB= new JButton("BWin");
 	JButton demoVictoryB= new JButton("BWin");
 	demoVictoryB.addActionListener(new ActionListener(){
 		public void actionPerformed(ActionEvent e)
@@ -117,7 +117,7 @@ public class Board extends JFrame implements MouseListener{
 		}
 	    });
 	pane.add(demoVictoryW);
-	*/
+	
 
 	if (checkB()==checkW()){
 	    v="DRAW";
